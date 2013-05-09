d8a2788fc01cd5510dabfa645b8a206d0ce15f9e
Darius Roberts <darius.roberts@gmail.com>
2013-05-09 13:07:57 -0400

    trying to use refinements, but cannot persist lvars in binding

--- lib/shatter.rb
+++ lib/shatter.rb
@@ -1,16 +1,35 @@
+require 'ostruct'
 module Shatter
-  def method_missing(method_id, *arguments)
-    @my_hash||={}
-    if @my_hash.has_key? method_id 
-      return @my_hash[method_id]
-    else
-      return super
+  module Delegator
+  	attr_writer :delegate
+
+  	def method_missing(*a)
+      if a.first.is_a?(Symbol) and @delegate.respond_to?(a.first)
+        return @delegate.send(*a) 
+      end
+      super
     end
   end
-  def shatter(my_hash)
-    @my_hash=my_hash
-    val=yield
-    @my_hash={}
-    val
-  end
+
+	refine Hash do
+	  def shatter(&blk)
+	  	blk.extend Delegator
+	  	blk.delegate = OpenStruct.new self
+      recursive_meta_fing_hack blk
+	  end
+
+	  #HACK inspiration: http://www.ruby-forum.com/topic/131910 
+    def recursive_meta_fing_hack(blk)
+      #preserve shadowed scope outside block
+    	b = blk.binding #.dup
+	  	each do |k,v|
+	  	  begin
+    	    return(b.instance_eval &blk)
+	      rescue NameError
+	      end
+	      b.eval "#{k}=#{v.inspect}"
+	    end
+      b.instance_eval &blk
+    end
+	end
 end
--- test/shatter_test.rb
+++ test/shatter_test.rb
@@ -1,5 +1,24 @@
 require 'test/unit'
-require '../lib/shatter'
+require_relative '../lib/shatter'
+
+using Shatter
+
+class ShatterTest < Test::Unit::TestCase
+	def test_basic
+		actual = {:one=>1,:two=>2,:three=>3}.shatter do
+			one + two + three
+		end
+		assert_equal expected, actual
+  end 
+  def test_conflict
+  	override = "world"
+		actual = {:message=>"world", :override=>"wrong"}.shatter do
+      "#{override} #{message}"
+		end
+		assert_equal "hello world", actual
+  end
+end
+=begin
 class ShatterTest < Test::Unit::TestCase
   def test_basic
     assert(TestBase.new.basic(:one=>1,:two=>2,:three=>3)==6)
@@ -8,8 +27,8 @@ class ShatterTest < Test::Unit::TestCase
     assert(TestBase.new.conflict(:message=>"world", :override=>"wrong")=="hello world")
   end
 end
+
 class TestBase
-  include Shatter
   def basic parameters={}
     shatter(parameters) do
       one + two + three
@@ -24,3 +43,4 @@ class TestBase
     "hello"
   end
 end
+=end
